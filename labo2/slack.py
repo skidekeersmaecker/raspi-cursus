6b03c5ef1aa37ce26e90f69fee409870d9e2559c
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-06-04 15:31:58 +0200

    Update slack.py

--- labo2/slack.py
+++ labo2/slack.py
@@ -4,7 +4,7 @@
 from slackclient import SlackClient
 import socket
 
-token = 'xoxp-81194043701-81197297509-190362477424-1e537c5d69a573b383e427b878cf2e17'
+token = 'token'
 tokendecode = token.decode('utf8')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)

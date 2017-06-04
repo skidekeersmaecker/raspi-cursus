140fc0945ad3538e2a6d5a6fd1ac67e12e49e0ef
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-06-04 15:32:13 +0200

    Update testSlack.py

--- labo2/testSlack.py
+++ labo2/testSlack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from slackclient import SlackClient
-token = "xoxp-81194043701-81197297509-191024182226-77338c03a42656fff129e35a59330f38"
+token = "token"
 sc = SlackClient(token)
 resp = sc.api_call(
         "chat.postMessage",
